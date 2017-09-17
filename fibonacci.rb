6b3658e84b3317cb522e49b12cb4799f628230f1
David Halewood <davidnhalewood@hotmail.com>
2017-09-17 16:24:02 +0100

    refactoring

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,9 +4,7 @@ def even_fib(m)
   while arr[-1] < m
     arr << arr[-1] + arr[-2]
   end
-  time1 = Time.now
   arr[0...-1].select {|x| x.even?}.inject(:+)
-  p time = Time.now - time1
 end
 
 even_fib(100)
@@ -14,13 +12,11 @@ even_fib(100)
 def even_fib2(m)
   sum = 0
   a, b = 0, 1
-  time1 = Time.now
   while b < m
     sum += b if b.even?
     a, b = b, a + b
   end
   sum
-  p time = Time.now - time1
 end
 
 even_fib2(100)
